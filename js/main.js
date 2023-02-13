6233becff20a5055d487d64fb3008984e602fc21
Simon Lau <simon.lau.sg@gmail.com>
2023-02-13 15:28:29 +0800

    managed to change screens

--- js/main.js
+++ js/main.js
@@ -1,9 +1,60 @@
 /*----- constants -----*/
 
 /*----- state variables -----*/
+const game = {
+  screen: "game",
+  board: [
+    ["0", "?", "?", "?"],
+    ["?", "?", "1", "?"],
+    ["?", "0", "?", "?"],
+    ["1", "0", "1", "?"],
+  ],
+};
 
 /*----- cached elements  -----*/
-
+const welcomeScreen = document.querySelector("#welcome");
+const gameScreen = document.querySelector("#game");
+const scoreScreen = document.querySelector("#score");
+const screens = [welcomeScreen, gameScreen, scoreScreen];
+const board = document.querySelector("tbody");
 /*----- event listeners -----*/
 
+function clickStartButton() {
+  game.screen = "game";
+  renderAll();
+}
+
+function clickScoreButton() {
+  game.screen = "score";
+  renderAll();
+}
+
 /*----- functions -----*/
+function renderScreen() {
+  // welcomeScreen.style.display = "none";
+  // gameScreen.style.display = "none";
+  // scoreScreen.style.display = "none"; //? block will show
+  screens.forEach((s) => s.classList.add("hide"));
+  const screen = document.querySelector("#" + game.screen);
+  screen.classList.remove("hide");
+}
+
+function buildBoardRow() {}
+
+function renderBoard() {}
+
+function renderAll() {
+  renderScreen();
+  renderBoard();
+}
+
+function main() {
+  const startButton = document.querySelector("#startButton");
+  startButton.addEventListener("click", clickStartButton);
+
+  const scoreButton = document.querySelector("#scoreButton");
+  scoreButton.addEventListener("click", clickScoreButton);
+  renderAll();
+}
+
+main();
--- readme.md
+++ readme.md
@@ -28,9 +28,22 @@
 * git add -A -> add all file to staging
 * git commit -m '...' -> commit 
 * git log --oneline
+* git push
 
 ### push an existing repository from the command line
 
 git remote add origin git@github.com:simonlau/connect4.git
 git branch -M main
 git push -u origin main
+
+### User Story
+
+* Start -> see start screen
+* Click start button -> see empty board
+* See an empty board when start the game
+* Click a column to drop a piece
+
+### Wireframe
+
+* 1 index.html
+* 1 main.js
